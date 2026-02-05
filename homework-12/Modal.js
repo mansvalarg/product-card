f3fcddaed85a4dd2bbf2be7cb6e5bfc4d5f74fcd
Mans Valarg <mans.valarg@gmail.com>
2026-02-05 12:31:22 +0300

    fixes and code optimization

--- homework-12/Modal.js
+++ homework-12/Modal.js
@@ -4,17 +4,23 @@ export class Modal {
     this.modal = document.getElementById(modalId);
     this.overlay = document.getElementById('overlay');
     this.#initOpen(buttonId);
-    this.#initClose(shouldCloseOnOverlay);
+    this.shouldCloseOnOverlay = shouldCloseOnOverlay;
   }
 
   open() {
     this.modal.classList.add('modal-showed');
     this.overlay.classList.add('overlay-showed');
+    this.#initClose(this.shouldCloseOnOverlay);
   }
 
   close() {
     this.modal.classList.remove('modal-showed');
     this.overlay.classList.remove('overlay-showed');
+    this.closeButton.removeEventListener('click', this.closeFunction);
+
+    if (this.shouldCloseOnOverlay) {
+      this.overlay.removeEventListener('click', this.closeFunction);
+    }
   }
 
   isOpen() {
@@ -22,36 +28,20 @@ export class Modal {
   }
 
   #initOpen(buttonId) {
-    const button = document.getElementById(buttonId);
-    button.addEventListener('click', () => {
-      this.open();
-    });
-    button.removeEventListener('click', () => {
-      this.open();
-    });
+    this.openButton = document.getElementById(buttonId);
+    this.openFunction = () => this.open();
+    this.openButton.addEventListener('click', this.openFunction);
   }
 
   #initClose(shouldCloseOnOverlay) {
-    const closeButton = this.modal.querySelector('#modal-close-button');
-
-    closeButton.addEventListener('click', () => {
-      this.close();
-    })
-    closeButton.removeEventListener('click', () => {
-      this.close();
-    });
+    this.closeButton = this.modal.querySelector('#modal-close-button');
+    this.closeFunction = () => this.close();
+    this.closeButton.removeEventListener('click', this.closeFunction);
+    this.closeButton.addEventListener('click', this.closeFunction);
 
     if (shouldCloseOnOverlay) {
-      this.overlay.addEventListener('click', () => {
-        if (this.modal.classList.contains('modal-showed')) {
-          this.close();
-        }
-      });
-      this.overlay.removeEventListener('click', () => {
-        if (this.modal.classList.contains('modal-showed')) {
-          this.close();
-        }
-      });
+      this.overlay.removeEventListener('click', this.closeFunction);
+      this.overlay.addEventListener('click', this.closeFunction);
     }
   }
 
